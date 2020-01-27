81949429a18660ddef21934934aeca7e91f7dc1a
Wikenbrant <55382118+Wikenbrant@users.noreply.github.com>
2020-01-27 20:44:10 +0100

    add

--- Inlmämningsuppgift2/Data/ApplicationDbContext.cs
+++ Inlmämningsuppgift2/Data/ApplicationDbContext.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Inlmämningsuppgift2.Models.User;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Inlmämningsuppgift2.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
